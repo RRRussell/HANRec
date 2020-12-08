711d70368517619fe61bc64b07671877f145c33b
ZIHENG DUAN <49986334+RRRussell@users.noreply.github.com>
2020-12-08 23:33:41 +0800

    Add files via upload

--- Aminer/model/Encoder.py
+++ Aminer/model/Encoder.py
@@ -7,101 +7,102 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import networkx as nx
+import numpy as np
 
+def get_ap_feature(path):
+    file = open(path, 'r', encoding='utf-8')
+    f = []
+    for line in file:
+        f.append(torch.tensor(np.array(list(line.split()),dtype=float), dtype=torch.float))
+    return f
 
 class Encoder(nn.Module):
 
-    def __init__(self, node2e, embed_dim, l1paths_list, l2paths_list, ua_list, va_list, l1aggr, l2aggr, cuda="cpu", uv=True):
-        """
-        :param node2e:
-        :param embed_dim:
-        :param l1paths_list:
-        [
-            array([ # node
-                [r1, n1], # level-1 path
-                [r1, n1],
-                [r1, n1],
-            ]),
-        ]
-        :param l2paths_list:
-        [
-            array([ # node
-                [r1, r2, n2], # level-2 path
-                [r1, r2, n2],
-                [r1, r2, n2],
-            ]),
-        ]
-        :param ua_list:
-        :param va_list:
-        :param l1aggr:
-        :param l2aggr:
-        :param cuda:
-        :param uv:
-        """
+    def __init__(self, cuda="cpu"):
+
         super(Encoder, self).__init__()
-        self.node2e = node2e
-        self.l1paths_list = l1paths_list
-        self.l2paths_list = l2paths_list
-        self.ua_list = ua_list
-        self.va_list = va_list
-        self.l1aggr = l1aggr
-        self.l2aggr =l2aggr
-        self.embed_dim = embed_dim
+
+        self.embed_dim = 300
         self.device = cuda
-        self.uv = uv
         self.linear1 = nn.Linear(3 * self.embed_dim, self.embed_dim)
         self.alpha = nn.Linear(2*self.embed_dim, 1)
         self.beta = nn.Linear(2*self.embed_dim, 1)
         self.gamma = nn.Linear(self.embed_dim, self.embed_dim)
+        self.num_author = 16604
+        self.num_paper = 12455
+
+        G = nx.read_gml('./link_pred_data/origin_data/clean_G.gml')
+        Neighbors = []
+        Neighbor = []
+        for node in G.nodes():
+            for nei in G.neighbors(node):
+                Neighbor.append(int(nei))
+            Neighbor.sort()
+            Neighbors.append(Neighbor)
+            Neighbor = []
+        self.Neighbors = Neighbors
+
+        a_f = get_ap_feature("./link_pred_data/origin_data/author_vectors_300.txt")
+        p_f = get_ap_feature("./link_pred_data/origin_data/paper_vectors_300.txt")
+        all_embeddings = a_f+p_f
+        self.all_embeddings = all_embeddings
 
     def forward(self, nodes):
         num_nodes = len(nodes)
-        nodes_l1paths = []
-        nodes_l2paths = []
-        """
-        nodes_l1n_attrs: 
-        [
-            [ # node
-                [attr1,attr2,attr3], # attributes of node's neighbor1
-                [attr1,attr5],
-            ]
-        ]
-        """
-        nodes_l1n_attrs = []
-        nodes_l2n_attrs = []
-
-        for i, node in enumerate(nodes):
-            nodes_l1paths.append(self.l1paths_list[node])
-            nodes_l2paths.append(self.l2paths_list[node])
-            if self.uv == True:
-                nodes_l1n_attrs.append([self.va_list[neighbor] for neighbor in nodes_l1paths[i][:,1]])
-                nodes_l2n_attrs.append([self.ua_list[neighbor] for neighbor in nodes_l2paths[i][:,2]])
+        all_combined = []
+        for node in nodes:
+            neighbors = self.Neighbors[node]
+            type_1 = []
+            type_2 = []
+            if node < self.num_author:
+                for neighbor in neighbors:
+                    if neighbor < self.num_author:
+                        type_1.append(self.all_embeddings[neighbor])
+                    else:
+                        type_2.append(self.all_embeddings[neighbor])
             else:
-                nodes_l1n_attrs.append([self.ua_list[neighbor] for neighbor in nodes_l1paths[i][:,1]])
-                nodes_l2n_attrs.append([self.va_list[neighbor] for neighbor in nodes_l2paths[i][:,2]])
+                for neighbor in neighbors:
+                    if neighbor >= self.num_author:
+                        type_1.append(self.all_embeddings[neighbor])
+                    else:
+                        type_2.append(self.all_embeddings[neighbor])
+            # print("type_1",len(type_1))
+            # print("type_2", len(type_2))
+            if len(type_1) != 0:
+                type_1_feat = torch.cat(type_1).reshape(-1, self.embed_dim).float()
+                type_1_feat = torch.mean(type_1_feat, 0).reshape(1, self.embed_dim)
+            else:
+                type_1_feat = torch.ones(1,self.embed_dim)
+            if len(type_2) != 0:
+                type_2_feat = torch.cat(type_2).reshape(-1, self.embed_dim).float()
+                type_2_feat = torch.mean(type_2_feat, 0).reshape(1, self.embed_dim)
+            else:
+                type_2_feat = torch.ones(1,self.embed_dim)
+            self_feats = self.all_embeddings[node].reshape(1, self.embed_dim)
 
-        l1neighs_feats = self.l1aggr.forward(nodes, nodes_l1paths, nodes_l1n_attrs)
-        l2neighs_feats = self.l2aggr.forward(nodes, nodes_l2paths, nodes_l2n_attrs)
-        self_feats = self.node2e.weight[nodes]
-        # self-connection could be considered.
-        # print("self_feats",self_feats)
-        # print("l1neighs_feats",l1neighs_feats)
-        # print("l2neighs_feats",l2neighs_feats)
-        # alphaa = torch.sum(self_feats*l1neighs_feats).reshape(-1,1)
-        # betaa = torch.sum(self_feats*l2neighs_feats).reshape(-1,1)
-        alphaa = F.prelu(self.alpha(torch.cat([self_feats, l1neighs_feats], dim=1)),torch.tensor(1).float())
-        betaa = F.prelu(self.beta(torch.cat([self_feats, l2neighs_feats], dim=1)),torch.tensor(1).float())
-        l1 = alphaa*l1neighs_feats
-        l2 = betaa*l2neighs_feats
-        combined = self_feats+l1+l2
+            # alphaa = F.prelu(self.alpha(torch.cat([self_feats, type_1_feat], dim=1)), torch.tensor(1).float())
+            # betaa = F.prelu(self.beta(torch.cat([self_feats, type_2_feat], dim=1)), torch.tensor(1).float())
+            # l1 = alphaa * type_1_feat
+            # l2 = betaa * type_2_feat
+            # print("typ1",type_1_feat.shape)
+            # print("typ2", type_2_feat.shape)
+            # print("self",self_feats.shape)
 
+            combined = torch.cat((self_feats,type_1_feat,type_2_feat)).reshape(1,-1).cuda()
+            combined = self.linear1(combined)
+            # combined = self_feats + l1 + l2
+            # combined = F.prelu(self.gamma(combined), torch.tensor(1).float())
+            all_combined.append(combined)
+
+        all_combined = torch.cat(all_combined)
         # print("l1:",l1)
         # print("l2:",l2)
         # combined = torch.cat([self_feats, l1neighs_feats, l2neighs_feats], dim=1) # why dim=1? neigh_fests and self_feats are in shape(128,64),
                                                                                   # containing features of a batch of 128 nodes.
         # print("combined before:",combined)
         # combined = F.prelu(self.linear1(combined),torch.tensor(1).float())
-        combined = F.prelu(self.gamma(combined),torch.tensor(1).float())
+
         # print("combined after:",combined)
 
-        return combined
+        return all_combined
--- Aminer/model/GATrec.py
+++ Aminer/model/GATrec.py
@@ -8,14 +8,29 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from model.utils import get_ap_feature, load_embedding
+
+def get_ap_feature(path):
+    file = open(path, 'r', encoding='utf-8')
+    f = []
+    for line in file:
+        f.append(torch.tensor(np.array(list(line.split()),dtype=float), dtype=torch.float))
+    return f
+
+def load_embedding(all_embeddings, nodes):
+    # temp = all_embeddings[(int(nodes[0]))]
+    temp = all_embeddings[str(int(nodes[0]))]
+    for i in range(1,len(nodes)):
+        temp = np.vstack((temp,all_embeddings[str(int(nodes[i]))]))
+        # temp = np.vstack((temp, all_embeddings[(int(nodes[i]))]))
+        
+    return torch.tensor(temp)
 
 class GATrec(nn.Module):
 
     def __init__(self, enc, embed_dim):
         super(GATrec, self).__init__()
         self.enc = enc
-        self.embed_dim = embed_dim
+        # self.embed_dim = embed_dim
 
         # self.w_1 = nn.Linear(self.embed_dim, 128)
         # self.w_2 = nn.Linear(128, 32)
@@ -26,8 +41,9 @@ class GATrec(nn.Module):
 
         proj_layers = []
         # D = embed_dim
-        D = 256
-        reduce_factor = 4
+        # D = 256
+        D = 600
+        reduce_factor = 8
         while D > 2:
             next_D = D // reduce_factor
             if next_D < 2:
@@ -35,7 +51,7 @@ class GATrec(nn.Module):
             linear_layer = nn.Linear(D, next_D, bias=True)
             nn.init.xavier_normal_(linear_layer.weight)
             proj_layers.append(linear_layer)
-            # proj_layers.append(nn.BatchNorm1d(next_D, momentum=0.5))
+            proj_layers.append(nn.BatchNorm1d(next_D, momentum=0.5))
             if next_D != 2:
                 proj_layers.append(nn.PReLU(1))
             D = next_D
@@ -49,21 +65,29 @@ class GATrec(nn.Module):
         self.criterion = nn.CrossEntropyLoss()
 
     def forward(self, nodes_u, nodes_v):
-
         embeds_u = self.enc(nodes_u)
         embeds_v = self.enc(nodes_v)
+        # embeds = self.enc(nodes)
 
         # a_f = get_ap_feature("./link_pred_data/origin_data/author_vectors_300.txt")
         # p_f = get_ap_feature("./link_pred_data/origin_data/paper_vectors_300.txt")
         # all_embeddings = a_f+p_f
 
-        # all_embeddings = np.load("./link_pred_data/baselines/node2vec.npy",allow_pickle=True).item()
+        # print("all_embeddings",len(all_embeddings))
+        # embeds_u = load_embedding(all_embeddings,list(nodes_u))
+        # embeds_v = load_embedding(all_embeddings,list(nodes_v+610))
+
+        # all_embeddings = np.load("./link_pred_data/baselines/deepwalk.npy",allow_pickle=True).item()
+        # all_embeddings = np.load("./link_pred_data/baselines/node2vec.npy", allow_pickle=True).item()
+        # all_embeddings = np.load("./link_pred_data/baselines/struc2vec.npy", allow_pickle=True).item()
+        # all_embeddings = np.load("./link_pred_data/baselines/sdne.npy", allow_pickle=True).item()
+        # all_embeddings = np.load("./link_pred_data/baselines/line.npy", allow_pickle=True).item()
         # embeds_u = load_embedding(all_embeddings, list(nodes_u))
         # embeds_v = load_embedding(all_embeddings, list(nodes_v))
 
         embeds = torch.cat((embeds_u,embeds_v),1)
         # print("embeds",embeds)
-        scores = embeds
+        scores = embeds.cuda()
         for proj_layer in self.proj_layers:
             scores = proj_layer(scores)
 
